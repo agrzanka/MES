366cc555004b79f48e951735e080bce28f9354d5
agrzanka <ag.grzanka@gmail.com>
2018-12-24 11:42:44 +0100

    derivatives (d N / d ksi) & (d N/ d eta) added to ShapeFunctions

--- ShapeFunctions.cpp
+++ ShapeFunctions.cpp
@@ -13,6 +13,17 @@ ShapeFunctions::ShapeFunctions(IntegrationPoints intPnts) //for four-nodes eleme
 		this->N[1][index] = 0.25*(1 + intPnts.ksi[index])*(1 - intPnts.eta[index]);
 		this->N[2][index] = 0.25*(1 + intPnts.ksi[index])*(1 + intPnts.eta[index]);
 		this->N[3][index] = 0.25*(1 - intPnts.ksi[index])*(1 + intPnts.eta[index]);
+
+		this->dNdKsi[0][index] = -0.25*(1 - intPnts.eta[index]);
+		this->dNdKsi[1][index] = 0.25*(1 - intPnts.eta[index]);
+		this->dNdKsi[2][index] = 0.25*(1 + intPnts.eta[index]);
+		this->dNdKsi[3][index] = -0.25*(1 + intPnts.eta[index]);
+
+		this->dNdEta[0][index] = -0.25*(1 - intPoints.ksi[index]);
+		this->dNdEta[1][index] = -0.25*(1 + intPoints.ksi[index]);
+		this->dNdEta[2][index] = 0.25*(1 + intPoints.ksi[index]);
+		this->dNdEta[3][index] = 0.25*(1 - intPoints.ksi[index]);
+
 	}
 }
 
@@ -22,5 +33,10 @@ void ShapeFunctions::showShapeFunctions()
 	{
 		cout << "\nN" << index + 1 << ":\n";
 		cout << N[index][0] << "\t" << N[index][1] << "\t" << N[index][2] << "\t" << N[index][3] << endl;
+		cout << "\n dN/dski:\n";
+		cout << dNdKsi[index][0] << "\t" << dNdKsi[index][1] << "\t" << dNdKsi[index][2] << "\t" << dNdKsi[index][3] << endl;
+		cout << "\n dN/dEta:\n";
+		cout << dNdEta[index][0] << "\t" << dNdEta[index][1] << "\t" << dNdEta[index][2] << "\t" << dNdEta[index][3] << endl;
+
 	}
 }
--- ShapeFunctions.h
+++ ShapeFunctions.h
@@ -8,8 +8,9 @@ public:
 	double N2[4];
 	double N3[4];
 	double N4[4];*/
-	double N[4][4];
-
+	double N[4][4];			// [shape function number][integration point number];
+	double dNdKsi[4][4];	// derivative (d N / d ksi) [shape function number][integration point number]
+	double dNdEta[4][4];	// derivative (d N / d eta) [shape function number][integration point number]
 
 	ShapeFunctions(IntegrationPoints intPoints);
 	void showShapeFunctions();
